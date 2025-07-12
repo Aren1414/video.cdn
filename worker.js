b5174119b01f50e743a9beffdeb318e2dea7cb5d
Aren <71951258+Aren1414@users.noreply.github.com>
2025-07-12 04:12:22 +0330

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,15 +1,13 @@
 export default {
   async fetch(request) {
-    const videoUrl = 'https://zxmqva22v53mlvaeypxc7nyw7ucxf5dat53l2ngf2umq6kiftn3a.arweave.net/zdkKg1qvdsXUBMPuL7cW_QVy9GCfdr00xdUZDykFm3Y';
-    const videoResponse = await fetch(videoUrl);
-
-    return new Response(videoResponse.body, {
-      status: 200,
+    const arweaveUrl = 'https://zxmqva22v53mlvaeypxc7nyw7ucxf5dat53l2ngf2umq6kiftn3a.arweave.net/zdkKg1qvdsXUBMPuL7cW_QVy9GCfdr00xdUZDykFm3Y';
+    const arweaveResponse = await fetch(arweaveUrl);
+    const contentType = arweaveResponse.headers.get('content-type') || 'video/mp4';
+    return new Response(arweaveResponse.body, {
       headers: {
-        'Content-Type': 'video/mp4',
-        'Cache-Control': 'public, max-age=31536000',
-        'Access-Control-Allow-Origin': '*',
-      }
+        'Content-Type': contentType,
+        'Cache-Control': 'public, max-age=31536000, immutable',
+      },
     });
-  }
-}
+  },
+};
